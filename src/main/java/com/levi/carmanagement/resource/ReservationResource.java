4244f0b16f1365c5699bd18e8400a773a8226b37
Levi <p.levente011230@gmail.com>
2023-02-04 17:09:06 +0100

    Update ReservationResource.java

--- src/main/java/com/levi/carmanagement/resource/ReservationResource.java
+++ src/main/java/com/levi/carmanagement/resource/ReservationResource.java
@@ -4,22 +4,25 @@ import com.levi.carmanagement.config.Secure;
 import com.levi.carmanagement.entity.Reservation;
 import com.levi.carmanagement.exception.NoAccessException;
 import com.levi.carmanagement.service.PersistenceService;
+import com.levi.carmanagement.service.QueryService;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 
 import javax.inject.Inject;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import java.net.URI;
+import java.util.Collection;
 
 @Path("user/reservations")
 public class ReservationResource {
 
     @Context
     UriInfo uriInfo;
+
+    @Inject
+    QueryService queryService;
 
     @Inject
     PersistenceService persistenceService;
@@ -30,5 +33,27 @@ public class ReservationResource {
         persistenceService.addReservation(carId, reservation);
         URI uri = uriInfo.getBaseUriBuilder().path("user/reservations").path(reservation.getId().toString()).build();
         return Response.ok().header("Location", uri).status(Response.Status.CREATED).build();
+    }
+
+    @GET
+    @Secure
+    @Path("{reservationId}")
+    public Response getReservationById(@PathParam("reservationId") Long reservationId) {
+        Reservation reservation = queryService.getReservationById(reservationId);
+        return Response.ok(reservation).status(Response.Status.OK).build();
+    }
+
+    @GET
+    @Secure
+    public Response getReservationsForCar(@QueryParam("carId") Long carId) {
+        Collection<Reservation> reservations = queryService.getReservationForCar(carId);
+        return Response.ok(reservations).status(Response.Status.OK).build();
+    }
+
+    @GET
+    @Secure
+    public Response getReservationsForUser() {
+        Collection<Reservation> reservations = queryService.getReservationForUser();
+        return Response.ok(reservations).status(Response.Status.OK).build();
     }
 }
